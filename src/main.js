9ee477286a32ebd8feb86a7131260d190f51474d
马博文 <1825643086@qq.com>
2022-01-13 00:19:33 +0800

    2022/1/13

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//产品的提示信息（开发时一般为否，发布时可以改成true）
+
 Vue.config.productionTip = false
+
+//.$mount('#app')就是直接挂载el没有区别
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
